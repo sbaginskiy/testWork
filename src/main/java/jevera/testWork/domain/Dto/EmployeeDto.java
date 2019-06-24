978b1232059dd2f149ca19f6c515eb6cd5d79f1d
sergy <sergey.bag.2016@gmail.com>
2019-06-24 21:07:37 +0300

    swap @Data on setter and getter

--- src/main/java/jevera/testWork/domain/Dto/EmployeeDto.java
+++ src/main/java/jevera/testWork/domain/Dto/EmployeeDto.java
@@ -1,23 +1,87 @@
 package jevera.testWork.domain.Dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDto {
     private String fullName;
     @JsonIgnore
     private String password;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateOfBirth;
     private String sex;
     private String nationality;
     private String workLocation;
     private String currentPosition;
     private String startYearOfProfExp;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getWorkLocation() {
+        return workLocation;
+    }
+
+    public void setWorkLocation(String workLocation) {
+        this.workLocation = workLocation;
+    }
+
+    public String getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(String currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    public String getStartYearOfProfExp() {
+        return startYearOfProfExp;
+    }
+
+    public void setStartYearOfProfExp(String startYearOfProfExp) {
+        this.startYearOfProfExp = startYearOfProfExp;
+    }
 }
